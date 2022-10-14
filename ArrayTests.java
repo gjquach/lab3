f34fae041d0206dc274007716ad1c18d2e266e40
gjquach <114312610+gjquach@users.noreply.github.com>
2022-10-13 22:10:33 -0700

    Array Method

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+  //New test cases with bugs
+  @Test
+  public void testReverseInPlace1() {
+    int[] input1 = {1, 2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{2, 1}, input1);
+	}
+
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {10};
+    assertArrayEquals(new int[]{10}, ArrayExamples.reversed(input1));
+  }
 }
